9aaf419676d5f1b2023974fd173ba4c9e5d97e18
Omar <istore@MacBook-Pro.local>
2022-05-20 23:03:16 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircle, faPen, faTrashCan
+} from '@fortawesome/free-solid-svg-icons'
+import './App.css';
+
+function App() {
+  return (
+    <div className="container App"> 
+
+<br></br>
+
+<h2>To Do List App</h2>
+
+<br></br>
+
+      <h1>testing</h1>
+    </div>
+  );
+}
+
+export default App;
