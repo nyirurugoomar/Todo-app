fdbea90458297356bb8215391a8b22bb4af6e5e8
nyirurugo omar <nyirurugoomar@gmail.com>
2022-05-25 13:15:09 +0200

    import all compenents to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React,{useState} from 'react';
+import {useState} from 'react';
 import AddTaskForm from './components/AddTaskForm.jsx';
 import ToDo from './components/ToDo.jsx';
 import UpdateForm from './components/UpdateForm.jsx';
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faCircleCheck, faPen, faTrashCan
-} from '@fortawesome/free-solid-svg-icons'
+
 import './App.css';
 
 function App() {
@@ -90,103 +87,32 @@ function App() {
         {/* //Update Task */}
         {updateData && updateData.title ? (
 
-          <>
-          <div className='row'>
-              <div className='col'>
-                <input 
-                value={ updateData && updateData.title}
-                onChange={(e) => changeTask(e)}
-                className='form-control form-control-lg'/>
-              </div>
-              <div className='col-auto'>
-                <button 
-                onClick={updateTask}
-                className='btn btn-lg btn-success mr-20'>
-                  Update
-                </button>
-                <buttom 
-                onClick={cancelUpdate}
-                className ="btn btn-lg btn-warning">
-                  Cancel
-                </buttom>
-              </div>
-        </div>
-        <br/>
-          </>
+          <UpdateForm 
+          updateData={updateData}s
+          changeTask={changeTask}
+          updateTask={updateTask}
+          cancelUpdate={cancelUpdate}
+          />
 
         ) : (
-          <>
-          {/* //Add Task */}
-        <div className='row'>
-              <div className='col'>
-                    <input 
-                    value={newTask}
-                    onChange={(e) => setNewTask(e.target.value)}
-                    className='form-control form-control-lg'/>
-                  </div>
-                  <div className='col-auto'>
-                    <button 
-                    onClick={addTask}
-                    className='btn btn-lg btn-success'>Add Task</button>
-                
-              </div>
-
-        </div>
-        <br/>
-          </>
+          <AddTaskForm
+          newTask={newTask}
+          setNewTask={setNewTask}
+          addTask={addTask}
+          />
+          
         )} 
         
 
        
 
-     {toDo && toDo.length ? '' : 'No Tasks...'}
-     {toDo && toDo
-     .sort((a,b)=>a.id>b.id ?1 : -1) // it keeps task on order
-     .map((task, index) =>{
-       return(
-         <React.Fragment key={task.id}>
-            
-          <div className="col taskBg">
-            <div className={task.status ? 'done' : ''}>
-            <span className='taskNumber'>{index +1}</span>
-            <span className='taskText'>{task.title}</span>
-
-            </div>
-              
-            <div className='iconsWrap'>
-              <span title = "Completed / Not Completed"
-              onClick={(e) =>markDone(task.id)}>
-                <FontAwesomeIcon icon={faCircleCheck}/>
-              </span>
-               
-               {task.status ? null :(
-                 <span title="Edit"
-                 onClick={() =>setUpdateData ({
-                   id:task.id,
-                   title:task.title,
-                   status:task.status ? true : false
-                   })}>
-                <FontAwesomeIcon icon={faPen}/>
-              </span>
-               )}
-              
-
-              <span title='Delete' 
-              onClick={() => deleteTask(task.id)}>
-              <FontAwesomeIcon icon={faTrashCan}/>
-              </span>
-
-            </div>
-
-          </div>
-          
-
-           
-
-         </React.Fragment>
-       )
-     })
-     }
+    {toDo && toDo.length ? '' : 'No Tasks...'}
+    <ToDo 
+    toDo={toDo}
+    markDone={markDone}
+    setUpdateData={setUpdateData}
+    deleteTask={deleteTask}
+    />
 
     </div>
   );
--- src/components/UpdateForm.jsx
+++ src/components/UpdateForm.jsx
@@ -1,5 +1,28 @@
-const UpdateForm =({ }) =>{
+const UpdateForm =({updateData,changeTask,updateTask,cancelUpdate }) =>{
     return(
+        <>
+          <div className='row'>
+              <div className='col'>
+                <input 
+                value={ updateData && updateData.title}
+                onChange={(e) => changeTask(e)}
+                className='form-control form-control-lg'/>
+              </div>
+              <div className='col-auto'>
+                <button 
+                onClick={updateTask}
+                className='btn btn-lg btn-success mr-20'>
+                  Update
+                </button>
+                <buttom 
+                onClick={cancelUpdate}
+                className ="btn btn-lg btn-warning">
+                  Cancel
+                </buttom>
+              </div>
+        </div>
+        <br/>
+          </>
 
     )
 }
